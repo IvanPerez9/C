ae61cfb95e2fb6955eb297b21322a95e2def72a0
IVÁN_P\Iván <ivan120695@gmail.com>
2019-11-18 16:57:25 +0100

    linkedList probado

--- Sudoku/linkedList.c
+++ Sudoku/linkedList.c
@@ -7,19 +7,61 @@ Linked list de ejemplo y funciones
 
 typedef struct node {
     int valor;
-    struct nodo * next;
+    struct node * next;
 } nodo;
+
+void add (nodo * cabecera , int value);
+void mostrarLista (nodo *cabecera);
+void addIni (nodo * cabecera, int val);
+void addIni2 (nodo ** cabecera, int val);
+void primero(nodo ** cabecera);
+void borrarUltimo (nodo * cabecera);
+void borrarIndex (nodo ** cabecera , int index);
+
 
 int main(int argc, char const *argv[])
 {
-    nodo * head = NULL; // Cabecera
-    head = malloc (sizeof(nodo));
-    if (head == NULL){
-        return 1;
-    }
+    struct node * head = NULL; // Cabecera
+    head = (struct node *) malloc (sizeof(struct node));
+    struct node * second = NULL; 
+    second = (struct node *) malloc (sizeof(struct node));
+    // Lo mismo pero con el typeDef
+    nodo * third = NULL; 
+    third = malloc (sizeof(nodo));
+
     head->valor = 1; // Acceso con -> como . en los punteros
-    head->next = NULL;
-    return 0;
+    head->next = second;
+    second->valor = 2;
+    second->next = third;
+    third->valor = 12;
+    third->next = NULL;
+
+
+    // Mostrar
+    mostrarLista (head);
+    printf("\n");
+    // Añadir nodo al final
+    add(head , 4);
+    // Añadir al principio -> Ojo el paso con & por ser puntero a puntero lo necesario
+    addIni2(&head , 11);
+    mostrarLista(head);
+    printf("\n");
+    // Eliminar el primer item de la lista
+    primero(&head);
+    mostrarLista(head);
+    printf("\n");
+    // Borrar por index
+    borrarIndex(&head, 3); // No empieza en 0
+    mostrarLista(head);
+    printf("\n");
+    // Borrar ultimo elemento
+    borrarUltimo(head);
+    mostrarLista(head);
+    printf("\n");
+    
+    free(head);
+    free(second);
+    free(third);
 }
 
 /*
@@ -30,6 +72,7 @@ void add (nodo * cabecera , int value){
     while (actual->next != NULL){
         actual = actual->next;
     }
+    // Distinto que en el main -> Aqui uso el typedef
     nodo *nuevoNodo;
     nuevoNodo = malloc(sizeof(nodo));
     nuevoNodo->next = NULL;
@@ -46,7 +89,7 @@ Imprimir todos los items de la lista
 void mostrarLista (nodo *cabecera){
     nodo * aux = cabecera;
 
-    while (aux->next != NULL){
+    while (aux != NULL){
         printf("%d \n" , aux->valor);
         aux = aux->next;
     }
@@ -64,7 +107,7 @@ void addIni (nodo * cabecera, int val){
 }
 
 /*
-Mover la cabecera
+Mover la cabecera, mejor que el anterior, mover el puntero
 */
 void addIni2 (nodo ** cabecera, int val){
     nodo *nuevoNodo;
@@ -112,15 +155,14 @@ void borrarIndex (nodo ** cabecera , int index){
     nodo * actual = *cabecera;
     nodo * aux = NULL;
     // Si es solo 1
-    if (index == 0){
+    if (index == 1){
         primero(cabecera);
     }
 
-    for (int i = 0; i < index-1; i++)
+    for (int i = 0; i < index; i++)
     {
         if (actual->next == NULL){
             perror("No he encontrado ese index \n");
-            return -1;
         }
         aux = actual;
         actual = actual->next;
